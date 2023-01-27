a395daf1de984d782ac52f146042065a75a36f35
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-01-28 00:18:33 +0100

    add data for About, Services and Tours

---
+++ src/data.js
@@ -0,0 +1,82 @@
+import tour1 from './images/tour1.webp'
+import tour2 from './images/tour2.webp'
+import tour3 from './images/tour3.webp'
+import tour4 from './images/tour4.webp'
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "Home" },
+  { id: 2, href: "#about", text: "About" },
+  { id: 3, href: "#services", text: "Services" },
+  { id: 4, href: "#tours", text: "Tours" },
+  { id: 5, href: "#featured", text: "featured" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    paragraphe:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    paragraphe:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    paragraphe:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    infos: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "china",
+    duration: "6 days",
+    price: "from $2100" 
+  },{
+    id: 2,
+    image: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    infos: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: "11 days",
+    price: "from $1400" 
+  },{
+    id: 3,
+    image: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    infos: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: "8 days",
+    price: "from $5000 "
+  },{
+    id: 4,
+    image: tour4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    infos: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "kenya",
+    duration: "20 days",
+    price: "from $3300" 
+  },
+];
